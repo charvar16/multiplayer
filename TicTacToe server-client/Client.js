e436d109c055fca81665bc8cf772d789b3d61769
charvar16 <proton74955ilo@protonmail.com>
2023-02-04 10:38:13 -0400

    adding testing folder

---
+++ TicTacToe server-client/Client.js
@@ -0,0 +1,118 @@
+import React, { useState } from 'react';
+import { useContext } from 'react';
+import {MoveContext} from './MoveContext';
+
+const xIsNext = true
+const winner = false
+
+function Square({ value, onSquareClick }) {
+  //const {roomEmpty} = useContext(MoveContext);
+  //const clickIfTwo = roomEmpty? null : onSquareClick
+
+  return (
+    //<button className="square" onClick={clickIfTwo}>
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+
+function Board({ xIsNext, squares }) {
+
+  return (
+    <React.Fragment>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default function Game() {
+  function handlePlay(nextSquares) {
+    // just emit the nextSquares array and to the server
+   
+  }
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+    </div>
+  );
+}
+
+
+
+
+
+
+
+
+// Server
+  export default function Game() {
+    const [history, setHistory] = useState([Array(9).fill(null)]);
+    const [currentMove, setCurrentMove] = useState(0);
+    const xIsNext = currentMove % 2 === 0;
+    const currentSquares = history[currentMove];
+
+    const winner = calculateWinner(squares); // Returns 'X' or 'O'
+    let status;
+
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else {
+    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+  }
+    function handleClick(i) {
+      if (calculateWinner(squares) || squares[i]) {
+        return;
+      }
+      const nextSquares = squares.slice();
+      if (xIsNext) {
+        nextSquares[i] = 'X';
+      } else {
+        nextSquares[i] = 'O';
+      }
+      handlePlay(nextSquares);
+    }
+
+    function handlePlay(nextSquares) {
+      const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+      setHistory(nextHistory);
+      setCurrentMove(nextHistory.length - 1);
+    }
+  }
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const path = require("path");
+//const { client, db } = require('./mongoConn');
+
+const { createServer } = require("http");
+const httpServer = createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(httpServer, { /* options */ });
+
+const openGames = []
+
+/* const clicks = db.collection('Clicks');
+  const newClick = {
+    ip : "despues de cambiar el package json"
+  } */
+
+app.use(express.static(path.join(__dirname, "client", "build")))
+
+async function main() {
+  //await client.connect().then(console.log("Atlas Mongo connection succesful"));
+  //  app.get('/', (req, res)   was originally here
+  io.on('connection', socket => {
+
+    app.get('/', (req, res) => {
+      
+      res.status(201).sendFile(path.join(__dirname, "client", "build"))
+    })
+    socket.emit('initBoard', openGames)
+    
+    socket.on('createdRoom', (room) =>{
+      openGames.push(room)
+      io.emit('updateGameBoard',openGames)
+    })
+
+    socket.on('joinRoom', (room) =>{
+      let updateOpponent = openGames.findIndex(obj => obj.creator === room)
+      openGames[updateOpponent].opponent = socket.id
+      io.emit('updateGameBoard',openGames)
+      socket.join(room)
+      })
+
+    socket.on('exitRoom', (creatorId) =>{
+        console.log('in closeRoom')
+      let roomIndex = openGames.findIndex(rooms => rooms.creator === creatorId)
+      openGames.splice(roomIndex, 1)
+      
+      io.emit('updateGameBoard',openGames)
+    })
+  });
+  httpServer.listen(port);
+  //await clicks.insertOne(newClick).then(()=>console.log("Added new document to Mongo database"))
+}
+
+main()
+  .then(console.log("Main promise fullfiled"))
+  .catch(console.error)
+//  .finally(() => client.close());
